1aa34b6264c7a87cd59c479d2c747df9454311fb
huangdi <531833998@qq.com>
2016-07-12 11:54:17 +0800

    OK

---
+++ src/Demo/Text.php
@@ -0,0 +1,12 @@
+<?php
+namespace KongQiHui\DemoText\text;
+class Text{
+	public function __construct()
+    {
+      echo '成功初始化';
+    }
+    public function hello(){
+    	echo '你好老板';
+    }
+}
+?>
